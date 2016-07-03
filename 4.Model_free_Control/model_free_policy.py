ca393407f540d9af19e7fec945a9ac104428c64c
Lebo <lebo1990@users.noreply.github.com>
2016-07-03 15:47:52 +0800

    Add files via upload

--- 4.Model_free_Control/model_free_policy.py
+++ 4.Model_free_Control/model_free_policy.py
@@ -1,6 +1,5 @@
 import MDP
 import random
-
 random.seed(0)
 
 grid = MDP.MDP()
@@ -60,7 +59,7 @@ def epsilon_greedy(qfunc, state, epsilon):
 ################ Different model free RL learning algorithms #####
 
 
-def MC(num_iter1, epsilon):
+def MC(num_iter1, epsilon, max_actions=100):
     J_hist = []
     n = dict()
     qfunc = dict()
@@ -71,20 +70,24 @@ def MC(num_iter1, epsilon):
 
     for iter1 in xrange(num_iter1):
         J_hist.append(compute_error(qfunc))
-
         s_sample = []
         a_sample = []
         r_sample = []
 
         s = random.choice(states)
         t = False
+        count = 0
         while t == False:
+            if count >= max_actions:
+                print 'Counts of continuous actions over %d, MC jump out' % max_actions
+                break
             a = epsilon_greedy(qfunc, s, epsilon)
             t, s1, r = grid.transform(s, a)
             s_sample.append(s)
             r_sample.append(r)
             a_sample.append(a)
             s = s1
+            count += 1
 
         g = 0.0
         for i in xrange(len(s_sample) - 1, -1, -1):
@@ -98,5 +101,74 @@ def MC(num_iter1, epsilon):
 
             g -= r_sample[i]
             g /= gamma
+
+    return qfunc, J_hist
+
+
+def SARSA(num_iter1, alpha, epsilon, max_actions=100):
+    J_hist = []
+    qfunc = dict()
+    for s in states:
+        for a in actions:
+            key = "%d_%s" % (s, a)
+            qfunc[key] = 0.0
+
+    for iter1 in xrange(num_iter1):
+        J_hist.append(compute_error(qfunc))
+        # Arbitrary Initialization
+        s = random.choice(states)
+        a = random.choice(actions)
+        t = False
+        count = 0
+        while t == False:
+            if count >= max_actions:
+                print 'Counts of continuous actions over %d, SARSA jump out' % max_actions
+                break
+            key = "%d_%s" % (s, a)
+            t, s_next, r = grid.transform(s, a)
+            a_next = epsilon_greedy(qfunc, s_next, epsilon)
+            key_next = "%d_%s" % (s_next, a_next)
+            qfunc[key] += alpha * (r + gamma * qfunc[key_next] - qfunc[key])
+            s = s_next
+            a = a_next
+            count += 1
+
+    return qfunc, J_hist
+
+
+def QLearning(num_iter1, alpha, epsilon, max_actions=100):
+    J_hist = []
+    qfunc = dict()
+    for s in states:
+        for a in actions:
+            key = "%d_%s" % (s, a)
+            qfunc[key] = 0.0
+
+    for iter1 in xrange(num_iter1):
+        J_hist.append(compute_error(qfunc))
+        # Arbitrary Initialization
+        s = random.choice(states)
+        a = random.choice(actions)
+        t = False
+        count = 0
+        while t == False:
+            if count >= max_actions:
+                print 'Counts of continuous actions over %d, QLearning jump out' % max_actions
+                break
+            key = "%d_%s" % (s, a)
+            t, s_next, r = grid.transform(s, a)
+            # Find next action through the max of Qt(St+1, a)
+            key_next = ""
+            qmax = -1.0
+            for a_n in actions:
+                key_next0 = "%d_%s" % (s_next, a_n)
+                if qmax < qfunc[key_next0]:
+                    qmax = qfunc[key_next0]
+                    key_next = key_next0
+
+            qfunc[key] += alpha * (r + gamma * qfunc[key_next] - qfunc[key])
+            s = s_next
+            a = epsilon_greedy(qfunc, s_next, epsilon)
+            count += 1
 
     return qfunc, J_hist
--- 4.Model_free_Control/test_model_free_policy.py
+++ 4.Model_free_Control/test_model_free_policy.py
@@ -1,7 +1,80 @@
-import model_free_policy
+from model_free_policy import *
 import matplotlib.pyplot as plt
 
-qfunc, J_hist = model_free_policy.MC(1000, 0.5)
-print qfunc
-plt.plot(J_hist)
+plt.figure(1)
+iterations = 5000
+alpha = 0.2
+epsilon = 0.2
+for itera in xrange(1, 5):
+    qfunc_MC, J_hist_MC = MC(iterations, epsilon)
+    plt.plot(J_hist_MC, '--', label='MC, iteration %d' % itera)
+    qfunc_SARSA, J_hist_SARSA = SARSA(iterations, alpha, epsilon)
+    plt.plot(J_hist_SARSA, ':', label='SARSA, iteration %d' % itera)
+    qfunc_QLearning, J_hist_QLearning = QLearning(iterations, alpha, epsilon)
+    plt.plot(J_hist_QLearning, label='QLearning, iteration %d' % itera)
+plt.legend()
+plt.xlabel('Iterations')
+plt.ylabel('Loss')
+plt.title('Conclusion: SARSA is the most random, Q-learning is stable')
+
+plt.figure(2)
+iterations = 5000
+EPSILON = [0.2, 0.5, 0.8]
+ALPHA = [0.2]
+for epsilon in EPSILON:
+    qfunc_MC, J_hist_MC = MC(iterations, epsilon)
+    plt.plot(J_hist_MC, '--', label='MC, epsilon = %.2f' % epsilon)
+    for alpha in ALPHA:
+        qfunc_SARSA, J_hist_SARSA = SARSA(iterations, alpha, epsilon)
+        plt.plot(J_hist_SARSA, ':', label='SARSA, alpha = %.2f, epsilon = %.2f' %
+                  (alpha, epsilon))
+        qfunc_QLearning, J_hist_QLearning = QLearning(
+            iterations, alpha, epsilon)
+        plt.plot(J_hist_QLearning, label='QLearning, alpha = %.2f, epsilon = %.2f' %
+                  (alpha, epsilon))
+plt.legend()
+plt.xlabel('Iterations')
+plt.ylabel('Loss')
+plt.title('Conclusion: Q-learning is off-policy, SARSA is unstable')
+
+plt.figure(3)
+iterations = 5000
+EPSILON = [0.2, 0.5]
+ALPHA = [0.2, 0.4]
+for epsilon in EPSILON:
+    qfunc_MC, J_hist_MC = MC(iterations, epsilon)
+    plt.plot(J_hist_MC, '--', label='MC, epsilon = %.2f' % epsilon)
+    for alpha in ALPHA:
+        qfunc_SARSA, J_hist_SARSA = SARSA(iterations, alpha, epsilon)
+        plt.plot(J_hist_SARSA, ':', label='SARSA, alpha = %.2f, epsilon = %.2f' %
+                  (alpha, epsilon))
+        qfunc_QLearning, J_hist_QLearning = QLearning(
+            iterations, alpha, epsilon)
+        plt.plot(J_hist_QLearning, label='QLearning, alpha = %.2f, epsilon = %.2f' %
+                  (alpha, epsilon))
+plt.legend()
+plt.xlabel('Iterations')
+plt.ylabel('Loss')
+plt.title('Conclusion: Q-learning is the best one')
+
+plt.figure(4)
+iterations = 5000
+ALPHA = [0.1, 0.3, 0.5, 0.8]
+epsilon = 0.2
+qfunc_MC, J_hist_MC = MC(iterations, epsilon)
+plt.plot(J_hist_MC, '--', label='MC, epsilon = %.2f' % epsilon)
+for alpha in ALPHA:
+    qfunc_SARSA, J_hist_SARSA = SARSA(iterations, alpha, epsilon)
+    plt.plot(J_hist_SARSA, ':', label='SARSA, alpha = %.2f, epsilon = %.2f' %
+              (alpha, epsilon))
+    qfunc_QLearning, J_hist_QLearning = QLearning(
+        iterations, alpha, epsilon)
+    plt.plot(J_hist_QLearning, label='QLearning, alpha = %.2f, epsilon = %.2f' %
+              (alpha, epsilon))
+plt.legend()
+plt.xlabel('Iterations')
+plt.ylabel('Loss')
+plt.title('Conclusion: Larger learning rate alpha make it faster convergence')
+
+
 plt.show()
